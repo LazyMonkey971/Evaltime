bbfb33bfd78cfa90df53cd6acc466f58fe35f68e
Slush-aux-bananes <labf0901@usherbrooke.com>
2025-04-19 12:30:03 -0400

    changer fonction

--- fct_fusion_csv_lep.R
+++ fct_fusion_csv_lep.R
@@ -2,15 +2,15 @@
 #Nom de la fonction: fusion_csv_lep
 library(data.table)
 
-fusion_csv_lep <- function(fichiers_dates) {
+fusion_csv_lep <- function(data1) {
   
   # Vérifier s'il y a des fichiers CSV
-  if (length(data) == 0) {
+  if (length(data1) == 0) {
     stop("Aucun fichier CSV trouvé dans le dossier spécifié.")
   }
   
   # Lire tous les fichiers CSV dans une liste de dataframes
-  named.list <- lapply(data[1:154], read.csv)
+  named.list <- lapply(data1[1:154], read.csv)
   
   # Fusionner les dataframes en un seul grand tableau
   lep <- rbindlist(named.list, use.names = FALSE)
