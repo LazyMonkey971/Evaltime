715da379775e4b0227d330a3ebc49d780d1d3100
juliannelemay <lemj2221@usherbrooke.ca>
2025-04-20 16:37:30 -0400

    renommer les scripts non utiles

--- fonctions_libidoptères/Figure_nb_sp_decennie.R
+++ fonctions_libidoptères/Figure_nb_sp_decennie.R
@@ -1,15 +1,6 @@
 #Figure nombre d'espèces observée par décennie au Québec 
 
-barplot(
-  nb_sp_par_decennie$nb_especes,
-  names.arg = nb_sp_par_decennie$decennie,
-  col = "orange",
-  main = "Nombre d'espèces observées par décennie au Québec",
-  xlab = "Décennie",
-  ylab = "Nombre d'espèces"
-)
-voir_sp_par_decennie()
-voir_sp_par_decennie <- function(data, color = "orange", title = "Nombre d'espèces observées par décennie au Québec") {
+fct_voir_sp_par_decennie <- function(data, color = "orange", title = "Nombre d'espèces observées par décennie au Québec") {
   barplot(
     data$nb_especes,
     names.arg = data$decennie,
