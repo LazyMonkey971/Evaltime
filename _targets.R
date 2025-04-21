dcfa0219349fd3ce6037328fe64a012fc0aa7040
Slush-aux-bananes <labf0901@usherbrooke.com>
2025-04-21 11:41:33 -0400

    Cibles plus propre

--- _targets.R
+++ _targets.R
@@ -13,13 +13,13 @@ tar_option_set(packages = c(
 
 list(
   tar_target(data, data_base()),
-  tar_target(lep, fusion_csv_lep(data)),
-  tar_target(invalid_names, verify_lep_names(lep, taxonomie)),
-  tar_target(lepi, uniformiser_val_nul(lep, "time_obs")),
-  tar_target(lepid, uniformiser_val_nul(lepi, "license")),
-  tar_target(lepido, retirer_heure_dwc_event_date(lepid, "dwc_event_date")),
-  tar_target(lepidop, uniformiser_obs_variable(lepido)),
-  tar_target(lepidopt, renommer_col_day_obs_en_month(lepidop)),
-  tar_target(lepidopte, retirer_colonne_na(lepidopt, "obs_unit")),
-  tar_target(lepidoptere, uniformiser_year_obs(lepidopte, "year_obs"))
+  tar_target(donnees_brutes, fusion_csv_lep(data)),
+  tar_target(invalid_names, verify_lep_names(donnees_brutes, taxonomie)),
+  tar_target(donnees_unif, uniformiser_val_nul(donnees_brutes, "time_obs")),
+  tar_target(donnees_plus_unif, uniformiser_val_nul(donnees_unif, "license")),
+  tar_target(donnees_pas_dheure, retirer_heure_dwc_event_date(donnees_plus_unif, "dwc_event_date")),
+  tar_target(donnees_correction_obs, uniformiser_obs_variable(donnees_pas_dheure)),
+  tar_target(correction_jour_mois, renommer_col_day_obs_en_month(donnees_correction_obs)),
+  tar_target(donnees_moins_colonne_na, retirer_colonne_na(correction_jour_mois, "obs_unit")),
+  tar_target(Donnees_propre, uniformiser_year_obs(donnees_moins_colonne_na, "year_obs"))
 )
