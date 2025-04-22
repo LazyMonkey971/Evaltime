0dd215ef843f96c6b93a02f775d761daf40ed6b5
juliannelemay <lemj2221@usherbrooke.ca>
2025-04-22 12:24:10 -0400

    modification de l'axe y de nb obs

--- fonctions_libidoptères/Figure_nb_obs_decennie.R
+++ fonctions_libidoptères/Figure_nb_obs_decennie.R
@@ -1,5 +1,7 @@
 # Fct figure nb obs qbc par décennie
 fct_nb_obs_qbc <- function(data) {
+  options(scipen = 999)
+  par(mgp = c(4, 1, 0))
   barplot(
     height = data$nb_observations,
     names.arg = data$decennie,
